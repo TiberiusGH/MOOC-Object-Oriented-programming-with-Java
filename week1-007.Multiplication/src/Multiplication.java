a32393060fdd34d9ec6b45aad645cdca149505c9
TiberiusGH <35294063+TiberiusGH@users.noreply.github.com>
2018-06-27 13:37:44 +0200

    Add files via upload

---
+++ week1-007.Multiplication/src/Multiplication.java
@@ -0,0 +1,13 @@
+public class Multiplication {
+
+    public static void main(String[] args) {
+
+        int X = 1337;
+        int Y = 42;
+        int Z = X*Y;
+
+        System.out.println(X + " * " + Y + " = " + Z);
+
+    }
+
+}
